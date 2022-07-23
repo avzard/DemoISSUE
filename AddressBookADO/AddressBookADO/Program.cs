4fa960101d2f96efe64220cc8d534f8f765c5330
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-23 20:48:10 +0530

    [demo]

---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBookADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.NET!");
+            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable");
+            int op = Convert.ToInt16(Console.ReadLine());
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            switch (op)
+            {
+                case 1:
+                    addressBookRepo.Create_Database();
+                    break;
+                case 2:
+                    addressBookRepo.CreateTables();
+                    break;
+
+            }
+        }
+    }
+}
